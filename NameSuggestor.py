63698c1ef716c0dcfb24e58f3a7b14ed8120dd02
Dylan <dahysen@email.wm.edu>
2015-04-06 14:55:20 -0400

    Skeleton Code Pt 2

--- NameSuggestor.py
+++ NameSuggestor.py
@@ -1,21 +1,42 @@
 import nltk
 import random
 
-#input
+# Will prompt user if they want to force a male or female name, if so outputs come from male.txt or female.txt instead of names.txt
+forceMale = False
+forceFemale = False
+
+ # Initialize list for user provided names. Both names from text files and from command line are listed in userNames.
+userNames = []
+
+# Initialize trait booleans. Will be set to true if user designates they should be on.
+isHeroOn = False
+isVillianOn = False
+isMasculineOn = False
+isFeminineOn = False
+
+
+# input: read text file
+# toDo: implement: Input text file and iterate over for capitalized words and/or words that match a list of names. Store in list.
+
+# input: names directly from user
+# toDo: implement: Read command line input from users. Store in list.
+
+# input: desired traits
+# toDo: implement: prompt user for which traits
 
 
 
-#trait: num 1 classifier
+# Machine learning using features specified
+# toDo: implement using code from class as base
+
+# Machine learning using inputted names from user
+# toDo: implement using code from class as base
+
+# Run classifier on 'names.txt' or male.txt/female.txt if specified by user.
+# toDo: implement ??
 
 
 
-#trait: num 2 classifier
+# output: names
 
-
-
-#input names classifier
-
-
-
-
-#output
+print 'hello'
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # NameSuggestor
+Concept: Use machine learning to suggest names to a user based on inputted names 
+and traits they desire in the name. Very practical for naming characters in a 
+story, or for looking for baby names.
+
+Implementation Overview: A python program that uses a naive bayes classifier in 
+NLTK, several input text files with lists of names, and the desired traits of 
+the character the user is naming. The classifier determines which features are 
+most important to the user by finding commonalities between user-provided names 
+and traits.
