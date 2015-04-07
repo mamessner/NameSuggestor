b3a8cbd8d45b120942866e90f66fc1928999ae56
Dylan <dahysen@email.wm.edu>
2015-04-07 12:39:09 -0400

    Skelton Code Pt 3

--- NameSuggestor.py
+++ NameSuggestor.py
@@ -1,42 +1,47 @@
+#python3
 import nltk
 import random
 
-# Will prompt user if they want to force a male or female name, if so outputs come from male.txt or female.txt instead of names.txt
-forceMale = False
-forceFemale = False
+def main() :
 
- # Initialize list for user provided names. Both names from text files and from command line are listed in userNames.
-userNames = []
+	# Will prompt user if they want to force a male or female name, if so outputs come from male.txt or female.txt instead of names.txt
+	forceMale = False
+	forceFemale = False
 
-# Initialize trait booleans. Will be set to true if user designates they should be on.
-isHeroOn = False
-isVillianOn = False
-isMasculineOn = False
-isFeminineOn = False
+	 # Initialize list for user provided names. Both names from text files and from command line are listed in userNames.
+	userNames = []
 
-
-# input: read text file
-# toDo: implement: Input text file and iterate over for capitalized words and/or words that match a list of names. Store in list.
-
-# input: names directly from user
-# toDo: implement: Read command line input from users. Store in list.
-
-# input: desired traits
-# toDo: implement: prompt user for which traits
+	# Initialize trait booleans. Will be set to true if user designates they should be on.
+	isHeroOn = False
+	isVillianOn = False
+	isMasculineOn = False
+	isFeminineOn = False
 
 
+	# input: read text file
+	# toDo: implement: Input text file and iterate over for capitalized words and/or words that match a list of names. Store in list.
 
-# Machine learning using features specified
-# toDo: implement using code from class as base
+	# input: names directly from user
+	# toDo: implement: Read command line input from users. Store in list.
 
-# Machine learning using inputted names from user
-# toDo: implement using code from class as base
-
-# Run classifier on 'names.txt' or male.txt/female.txt if specified by user.
-# toDo: implement ??
+	# input: desired traits
+	# toDo: implement: prompt user for which traits
 
 
 
-# output: names
+	# Machine learning using features specified
+	# toDo: implement using code from class as base
 
-print 'hello'
+	# Machine learning using inputted names from user
+	# toDo: implement using code from class as base
+
+	# Run classifier on 'names.txt' or male.txt/female.txt if specified by user.
+	# toDo: implement ??
+
+
+
+	# output: names
+
+	print ('hello')
+
+main()
